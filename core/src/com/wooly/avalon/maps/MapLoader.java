9802ed51af0d021dc10334c25849c2ccbff5a6a3
WoolyMamooth <kispeterzsm@gmail.com>
2024-02-17 15:02:07 +0100

    maps are now loaded from tsv files

--- core/src/com/wooly/avalon/maps/MapLoader.java
+++ core/src/com/wooly/avalon/maps/MapLoader.java
@@ -8,18 +8,35 @@ import static com.wooly.avalon.TDGame.TEXTURE_EXTENSION;
 import static com.wooly.avalon.TDGame.place;
 import static com.wooly.avalon.TDGame.player;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.Texture;
 import com.wooly.avalon.TDGame;
 import com.wooly.avalon.units.heroes.ArthurPendragon;
 import com.wooly.avalon.units.heroes.Hero;
 import com.wooly.avalon.units.heroes.Mordred;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Dictionary;
+import java.util.Hashtable;
+import java.util.List;
 import java.util.Objects;
 
 public class MapLoader {
     //responsible for loading in the map data
     //still needs a lot of work
     TDGame game;
+    String mapdataPath="mapdata/map"; //add mapID and .tsv to the end
+    /**
+     * Keeps track of what data is in which line of the data file.
+     */
+    Dictionary<String,Integer> mapdataLines=new Hashtable<>();
+    /**
+     * The data from the map is stored here, and the different "load" functions read
+     * from here.
+     */
+    String[] mapdata;
     /**
      * Responsible for setting the difficulty of the game. The levels are:
      * 0-Easy,
@@ -28,18 +45,33 @@ public class MapLoader {
      * 3-Extreme,
      * 4-Nightmare
      */
+    String mapdataDelimiter="\t";
     public static int GAME_DIFFICULTY =4;
     public MapLoader(TDGame game) {
         this.game=game;
+
+        mapdataLines.put("mapName",0);
+        mapdataLines.put("pathX",1);
+        mapdataLines.put("pathY",2);
+        mapdataLines.put("enemyNumber",3);
+        mapdataLines.put("enemyName",4);
+        mapdataLines.put("enemyDelay",5);
+        mapdataLines.put("groupDelay",6);
+        mapdataLines.put("towerX",7);
+        mapdataLines.put("towerY",8);
     }
 
     public TDMap getMap(int mapID){
         System.out.println("MAPLOADER LOADING");
+        FileHandle fileHandle=Gdx.files.local(mapdataPath+mapID+".tsv");
+        mapdata=fileHandle.readString().split("\n");
+        System.out.println(Arrays.toString(mapdata));
+
         Texture backgroundTexture= loadBackgroundTexture(mapID); //loads texture for background
-        Path path = loadPath(mapID); //loads path that enemies will follow
-        String[] enemiesToSpawn=loadEnemiesToSpawn(mapID); //loads enemies that will be spawned
-        Float[] enemiesSpawnDelay=loadSpawnDelay(mapID); //loads the delay between enemy spawns
-        TowerSpace[] towerSpaces=loadTowerLocations(mapID); //loads the buildable spaces
+        Path path = loadPath(); //loads path that enemies will follow
+        String[] enemiesToSpawn=loadEnemiesToSpawn(); //loads enemies that will be spawned
+        Float[] enemiesSpawnDelay=loadSpawnDelay(); //loads the delay between enemy spawns
+        TowerSpace[] towerSpaces=loadTowerLocations(); //loads the buildable spaces
         if(Objects.equals(player.getEquippedHero(), "None")) {
             return new TDMap(mapID, backgroundTexture, path, enemiesToSpawn, enemiesSpawnDelay, towerSpaces);
         }else{
@@ -47,7 +79,6 @@ public class MapLoader {
             return new TDMap(mapID, backgroundTexture, path, enemiesToSpawn, enemiesSpawnDelay, towerSpaces,hero);
         }
     }
-
     private Hero loadHero(String heroName) {
         Coordinate spawnPos=SCREEN_CENTER;
         //TODO
@@ -59,64 +90,69 @@ public class MapLoader {
                 return new ArthurPendragon(spawnPos);
         }
     }
-
     private Texture loadBackgroundTexture(int mapID){
         System.out.println("TEXTURE: map_assets/backgrounds/"+mapID+TEXTURE_EXTENSION);
         return TDGame.fetchTexture("map_assets/backgrounds/"+mapID);
     }
-    private Path loadPath(int mapID){
-        //TODO search database for path/paths of map by ID
-        //for now we just return a path from top left to bottom right
+    private Path loadPath(){
         //slight issue: top left corner is (0,SCREEN_HEIGHT), not (0,0)
         //so we subtract the y coordinate so it's not flipped
-        Path path=new Path(new Coordinate[]{
-                new Coordinate(SCREEN_TOP_LEFT.x(), SCREEN_TOP_LEFT.y()),
-                place(543, SCREEN_HEIGHT-497),
-                place(1122, SCREEN_HEIGHT-658),
-                place(1691, SCREEN_HEIGHT-820),
-                place(1919, SCREEN_HEIGHT-839),
-                place(SCREEN_BOT_RIGHT.x(), SCREEN_BOT_RIGHT.y())
-        });
+        String[] xData=mapdata[mapdataLines.get("pathX")].split(mapdataDelimiter);
+        String[] yData=mapdata[mapdataLines.get("pathY")].split(mapdataDelimiter);
+        Coordinate[] coords=new Coordinate[xData.length];
+        for (int i = 0; i < xData.length; i++) {
+            coords[i]=new Coordinate(Float.parseFloat(xData[i]),SCREEN_HEIGHT-Float.parseFloat(yData[i]));
+        }
+        Path path=new Path(coords);
         System.out.println("PATH:"+path);
         return path;
     }
-    private String[] loadEnemiesToSpawn(int mapID){
-        //TODO search database for enemies to spawn by ID
-        System.out.println("ENEMIES: default");
-        String[] spawns= new String[100];
-        for (int i = 0; i < 100; i++) {
-            if(i%10==0){
-                spawns[i]="ogre";
-            }else {
-                spawns[i]="goblin";
+    private String[] loadEnemiesToSpawn(){
+        String[] enemyNumbersStr=mapdata[mapdataLines.get("enemyNumber")].split(mapdataDelimiter);
+        String[] enemyNames=mapdata[mapdataLines.get("enemyName")].split(mapdataDelimiter);
+        List<String> spawns=new ArrayList<String>();
+        for (int i = 0; i < enemyNames.length; i++) {
+            for (int j = 0; j < Float.parseFloat(enemyNumbersStr[i]); j++) {
+                spawns.add(enemyNames[i].trim());
             }
         }
-        return spawns;
+        String[] res= new String[spawns.size()];
+        for (int i = 0; i < res.length; i++) {
+            res[i]= spawns.get(i);
+        }
+        return res;
     }
-    private  Float[] loadSpawnDelay(int mapID){
-        //TODO search database for spawn delay
-        System.out.println("ENEMY DELAYS: default");
-        Float[] delays= new Float[100];
-        for (int i = 0; i < 100; i++) {
-            if(i%10==0){
-                delays[i]=10f;
-            }else {
-                delays[i] = 1f;
+    private  Float[] loadSpawnDelay(){
+        String[] enemyNumbersStr=mapdata[mapdataLines.get("enemyNumber")].split(mapdataDelimiter);
+
+        String[] delaysStr=mapdata[mapdataLines.get("enemyDelay")].split(mapdataDelimiter);
+        String[] groupDelays=mapdata[mapdataLines.get("groupDelay")].split(mapdataDelimiter);
+
+        List<String> delays=new ArrayList<>();
+        for (int i = 0; i < enemyNumbersStr.length; i++) {
+            delays.add(groupDelays[i]);
+            for (int j = 1; j < Float.parseFloat(enemyNumbersStr[i]); j++) {
+                delays.add(delaysStr[i]);
             }
         }
-        return delays;
+
+        Float[] res=new Float[delays.size()];
+        for (int i = 0; i < res.length; i++) {
+            res[i]= Float.valueOf(delays.get(i));
+        }
+
+        return res;
     }
     private TowerSpace towerSpaceByCoordinate(Coordinate coordinate){
         return new TowerSpace(coordinate,TDGame.fetchTexture("towers/towerButtons/actives/towerspace"),TDGame.fetchTexture("towers/towerButtons/inactives/towerspace"));
     }
-    private TowerSpace[] loadTowerLocations(int mapID){
-        //TODO search database for tower locations
-        System.out.println("TOWERSPACES: default");
-        //Check loadPath for info on SCREEN_HEIGHT-x issue
-        return new TowerSpace[]{
-            towerSpaceByCoordinate(place(100,SCREEN_HEIGHT-300)),
-            towerSpaceByCoordinate(place(300,SCREEN_HEIGHT-510)),
-            towerSpaceByCoordinate(place(900,SCREEN_HEIGHT-600))
-        };
+    private TowerSpace[] loadTowerLocations(){
+        String[] xData=mapdata[mapdataLines.get("towerX")].split(mapdataDelimiter);
+        String[] yData=mapdata[mapdataLines.get("towerY")].split(mapdataDelimiter);
+        TowerSpace[] spaces= new TowerSpace[xData.length];
+        for (int i = 0; i < xData.length; i++) {
+            spaces[i]=towerSpaceByCoordinate(new Coordinate(Float.parseFloat(xData[i]),SCREEN_HEIGHT-Float.parseFloat(yData[i])));
+        }
+        return spaces;
     }
 }
